3fa75c17ebfe6fdc3b791596129b1db2db10958b
Dan Kelly <dpk5e7@yahoo.com>
2022-11-12 00:39:37 -0700

    refactor with graphql

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from "@apollo/client";
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import { SAVE_BOOK } from "../utils/mutations";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -19,6 +22,8 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
+
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -65,11 +70,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await saveBook({
+        variables: { ...bookToSave },
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,58 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { decode } = require("jwt-decode");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      return await User.findOne({ _id: context.user._id });
+    },
+    users: async () => {
+      return await User.find({});
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({
+        $or: [{ username: email }, { email: email }],
+      });
+      if (!user) {
+        return { message: "Can't find this user" };
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        return { message: "Wrong password!" };
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, args, context) => {
+      return await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: { ...args } } },
+        { new: true, runValidators: true }
+      );
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      return await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
